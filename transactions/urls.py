762d380bea27622f1e0286d8e815292b5fb3c391
mehedi0105 <mehedi01014@gmail.com>
2024-10-05 23:29:28 +0600

    changes

--- transactions/urls.py
+++ transactions/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('withdraw/', views.Withdraw.as_view(), name='withdraw'),
     path('transfer_money/', views.TransferMoney.as_view(), name='transfer_money'),
     path('loan_request/', views.Loan.as_view(), name='loan_request'),
-    path('loan_approve/<int:id>', views.Loan.as_view(), name='loan_approve'),
+    path('loan_approve/<int:id>/', views.Loan.as_view(), name='loan_approve'),
     path('user_transaction/<int:id>/', views.UserTransaction.as_view(), name='user_transaction'),
     # path('deposit/', views.YourView.as_view(), name='deposit'),
 ]
